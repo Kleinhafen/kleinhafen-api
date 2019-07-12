1c15dc373ef632f6b6bd4d84073675f646e72ae4
Vlad-Ștefan Harbuz <vlad@vladh.net>
2019-07-12 22:49:54 +0200

    Add API prefix

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded())
 
-app.post('/interest-form-message', (req, res, next) => {
+app.post('/api/interest-form-message', (req, res, next) => {
   console.log(req.body)
   res.json({status: 'ok'})
   res.end()
